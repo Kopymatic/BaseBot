1ffb97bf6dff267590218550e1f6e5e328fa38e3
Kopy <creepcipher@gmail.com>
2022-03-05 22:40:24 -0700

    export commandstats

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,3 +11,4 @@ export {
 } from "./utils/CommandUtils";
 export { default as InteractionUtils } from "./utils/InteractionUtils";
 export { default as SlashCommand } from "./utils/SlashCommand";
+export { default as CommandStats } from "./CommandStats";
